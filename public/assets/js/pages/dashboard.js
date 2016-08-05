ce39d2351af5c5c4474ed9d46a9da8cfe7b099c2
Daku <admin@codeanimu.net>
2016-08-05 16:49:51 +0100

    move timer js to dashboard only

--- public/assets/js/pages/dashboard.js
+++ public/assets/js/pages/dashboard.js
@@ -2,6 +2,7 @@ $(function(){
 	"use strict";
 
 	//Sort by unread, then alphabetically.
+	//We sort by unread by using prepending 1 or 0 depending if it's unread or not to an invisible td
 	$(".tablesorter").tablesorter({
 		sortList: [[0,0], [1,0]]
 	});
@@ -208,6 +209,28 @@ $(function(){
 			}
 		}
 	});
+
+
+	//Initialize header update timer
+	var timer_obj = $('#update-timer'),
+	    timer_arr = timer_obj.text().split(':'),
+	    time_left = parseInt(timer_arr[0] * 60 * 60, 10) + parseInt(timer_arr[1] * 60, 10) + parseInt(timer_arr[2], 10);
+	var timer = setInterval(function() {
+		var hours   = parseInt(time_left / 60 / 60, 10).toString(),
+		    minutes = parseInt(time_left / 60 % 60, 10).toString(),
+		    seconds = parseInt(time_left % 60, 10).toString();
+
+		if(  hours.length == 1) hours   = '0'+hours;
+		if(minutes.length == 1) minutes = '0'+minutes;
+		if(seconds.length == 1) seconds = '0'+seconds;
+
+		timer_obj.text(hours + ':' + minutes + ':' + seconds);
+
+		if(--time_left < 0) {
+			clearInterval(timer);
+			//location.reload(); //?
+		}
+	}, 1000);
 
 	/* http://stackoverflow.com/a/3710226/1168377 */
 	function isJsonString(str) {
