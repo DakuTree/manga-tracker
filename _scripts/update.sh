e6142ace98b6a4e19b65bd264a1ec4cbe33d7215
Daku <admin@codeanimu.net>
2016-08-16 13:00:26 +0100

    Massive whoops, this was targetting /root instead of /www

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -2,7 +2,7 @@
 
 #Get KissManga CookieLib
 sudo python /var/www/tracker.codeanimu.net/dev/public_html/_scripts/get_kissmanga_cookie.py
-sudo chown www-data:www-data ./ --recursive
+sudo chown www-data:www-data /var/www/tracker.codeanimu.net/ --recursive
 
 #Run update
 php /var/www/tracker.codeanimu.net/dev/public_html/public/index.php admin/update_titles
