a7e0629d356282c3e5970301bb09fc78f894ad5a
Daku <admin@codeanimu.net>
2016-08-18 18:54:40 +0100

    seeResponseCode apparently doesn't work with AcceptanceTester

--- application/tests_codeception/acceptance/FrontPageCept.php
+++ application/tests_codeception/acceptance/FrontPageCept.php
@@ -3,7 +3,6 @@
 
 	$I->wantTo('Check the Front Page works (No login)');
 	$I->amOnPage('/');
-	$I->seeResponseCodeIs(200);
 	$I->see('FRONT PAGE');
 	$I->see('©2016 tracker.codeanimu.net');
 	$I->see('Login');
