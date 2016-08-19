d765bfe66b774d797be1800a8529a2c73b85ea37
Daku <admin@codeanimu.net>
2016-08-19 14:00:23 +0100

    fix test

--- application/tests_codeception/acceptance/AboutCept.php
+++ application/tests_codeception/acceptance/AboutCept.php
@@ -3,6 +3,6 @@
 
 	$I->wantTo('Check the About Page works');
 	$I->amOnPage('/about');
-	$I->see('This is a site dedicated to tracking manga across multiple sites.');
+	$I->see('This is a site dedicated to tracking manga across multiple aggregate sites.');
 	$I->see('©2016 tracker.codeanimu.net');
 	$I->seeInTitle('Manga Tracker - About');
