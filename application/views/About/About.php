a7fcd24d703cb428ce1845fd7da5cc2ae90a4693
Daku <admin@codeanimu.net>
2016-08-19 13:59:47 +0100

    Rewording of stuff

--- application/views/About/About.php
+++ application/views/About/About.php
@@ -1,7 +1,6 @@
 <header>
 	<h2>Manga Tracker</h2>
-	<p>This is a site dedicated to tracking manga across multiple sites.</p>
-	<p>The site currently supports: Mangafox & MangaHere.</p>
+	<p>This is a site dedicated to tracking manga across multiple aggregate sites.</p>
 </header>
 
 <article>
@@ -10,9 +9,9 @@
 
 	<h3>What the site is built on</h3>
 	<p>
-		The site is built using: PHP7, MySQL5.6 and Codeigniter 3.
+		The site is built using: PHP7, MySQL5.6 and CodeIgniter 3. Behind the scenes we also use: TravisCI, ScrutinizerCI, Composer.
 		<br>
-		Behind the scenes we also use: TravisCI, ScrutinizerCI, Composer.
+		A more complete list can be found in our <a href="<?=base_url('humans.txt')?>">humans.txt</a>.
 		<br>
 		At the moment the site is closed-source, but there is plans to go open-source soon.
 	</p>
@@ -22,10 +21,11 @@
 		<a href="https://github.com/AllMangasReader-dev/AMR">AllMangasReader</a>, for the initial idea, as well as the base of topbar & site-specific code.
 		<br>
 		<a href="http://www.novelupdates.com/">Novel Updates</a>, for improving on the idea of a server-side tracker (and actually doing it well!). Also for the tracker look/design.
+		<br>
 	</p>
 
 	<h3>Contact</h3>
 	<p>
-		If you need contact me for whatever reason, send me an email at <a href="mailto:admin@codeanimu.net">admin@codeanimu.net</a> or twitter at <a href="https://twitter.com/Avareei">@Avareei</a>.
+		If you need contact me for whatever reason, send me an email at <a href="mailto:admin@codeanimu.net">admin@codeanimu.net</a> or DM me on twitter at <a href="https://twitter.com/Avareei">@Avareei</a>.
 	</p>
 </article>
