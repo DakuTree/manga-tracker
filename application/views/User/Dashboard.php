aac083046bad729128021064ae74a4a8441e2354
Daku <admin@codeanimu.net>
2016-08-20 22:17:46 +0100

    update info

--- application/views/User/Dashboard.php
+++ application/views/User/Dashboard.php
@@ -10,7 +10,7 @@
 		</ul>
 		<div id="update-timer-container" class="pull-right">
 			Next update in: <span id="update-timer"><?=$this->Tracker->getNextUpdateTime()?></span>
-			<i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Each series is updated at different times, but only once every 16 hours.<br>This is to avoid bombarding the sites with 100s of requests all at once."></i>
+			<i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Each series is updated at different times, but only once every 14 hours.<br>This is to avoid bombarding the sites with 100s of requests all at once."></i>
 		</div>
 	</nav>
 
