3d7fb4f32cac5abea8b02dc13402fb212f4fab70
Daku <admin@codeanimu.net>
2016-09-01 20:45:15 +0100

    run update as www-data

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -9,4 +9,4 @@ sudo python /var/www/trackr.moe/dev/public_html/_scripts/get_kissmanga_cookie.py
 sudo chown www-data:www-data /var/www/trackr.moe/ --recursive
 
 #Run update
-php /var/www/trackr.moe/dev/public_html/public/index.php admin/update_titles
+sudo -u www-data php /var/www/trackr.moe/dev/public_html/public/index.php admin/update_titles
