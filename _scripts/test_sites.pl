6273a3934046593579b76e4923438678b664f0ec
Daku <admin@codeanimu.net>
2016-09-14 16:48:45 +0100

    grab folder via executable dir, instead of config this should make the script work on both dev & prod

--- _scripts/test_sites.pl
+++ _scripts/test_sites.pl
@@ -2,6 +2,8 @@
 
 ##### MODULES  #####{
 use File::Basename;
+use Cwd qw(abs_path);
+
 use Config::Simple; #for reading config, variables etc
 
 use strict;
@@ -12,15 +14,18 @@ use warnings;
 if($> != 0) { die("Script must be run as root!"); }
 
 ##### CORE VARIABLES #####{
-my ($trackr_prod, $testing_email);
+my ($testing_email);
+
+my $dirname = dirname(abs_path(__FILE__));
+if(!($dirname =~ /\/public_html\/_scripts$/)) { die("This is being run in an invalid location?"); }
+my $trackrLocation = ($dirname =~ s/\/_scripts$//r);
 
 my $cfgLocation = dirname(__FILE__)."/config.ini";
 if(!(-e $cfgLocation)) { die("Config file does not exist. Did you forgot to rename the default config?\n"); }
 my $cfg = new Config::Simple($cfgLocation) or die Config::Simple->error();
-$trackr_prod   = $cfg->param('general.trackr_prod') =~ s/\/?$//r;
 $testing_email = $cfg->param('testing.email');
 
-if(!length $trackr_prod || !length $testing_email) { die("Config params are empty??\n"); }
+if(!length $testing_email) { die("Config params are empty??\n"); }
 #####################}
 
 open STDERR, ">>", "/var/log/perl-error.log" or die "Can't open file for STDERR";
@@ -28,7 +33,7 @@ open STDERR, ">>", "/var/log/perl-error.log" or die "Can't open file for STDERR"
 ###### SCRIPT ######{
 
 #Run the tests and grab the output
-my $test_output = `php -dxdebug.profiler_enable=off ${trackr_prod}/vendor/phpunit/phpunit/phpunit --bootstrap ${trackr_prod}/application/tests/Bootstrap.php --configuration ${trackr_prod}/application/tests/phpunit.xml Site_Model_test ${trackr_prod}/application/tests/models/Sites_Model_test.php`;
+my $test_output = `php -dxdebug.profiler_enable=off ${trackrLocation}/vendor/phpunit/phpunit/phpunit --bootstrap ${trackrLocation}/application/tests/Bootstrap.php --configuration ${trackrLocation}/application/tests/phpunit.xml Site_Model_test ${trackrLocation}/application/tests/models/Sites_Model_test.php`;
 
 if(index($test_output, "FAILURES") == -1) {
 	system("mailx -s 'Trackr.moe DailyTest: SUCCESS' < /dev/null '${testing_email}' &");
