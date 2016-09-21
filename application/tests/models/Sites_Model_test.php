d314c4fad03158cc251000c0fa1059bfda005d71
Daku <admin@codeanimu.net>
2016-09-21 11:18:37 +0100

    test GameOfScanlation class

--- application/tests/models/Sites_Model_test.php
+++ application/tests/models/Sites_Model_test.php
@@ -129,4 +129,16 @@ class Site_Model_test extends TestCase {
 		$this->assertRegExp('/^[a-z]+\/[0-9]+\/[0-9]+(?:\/[0-9]+)?$/', $result['latest_chapter']);
 		$this->assertRegExp('/^[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+$/', $result['last_updated']);
 	}
+	public function test_GameOfScanlation() {
+		$result = $this->Sites_Model->{'GameOfScanlation'}->getTitleData('legendary-moonlight-sculptor.99');
+
+		$this->assertInternalType('array', $result);
+		$this->assertArrayHasKey('title', $result);
+		$this->assertArrayHasKey('latest_chapter', $result);
+		$this->assertArrayHasKey('last_updated', $result);
+
+		$this->assertEquals('Legendary Moonlight Sculptor', $result['title']);
+		$this->assertRegExp('/^[a-z0-9\.-]+$/', $result['latest_chapter']);
+		$this->assertRegExp('/^[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+$/', $result['last_updated']);
+	}
 }
