cf4d8cb587c94e23a86648a38adc3ad34286d4fb
Daku <admin@codeanimu.net>
2016-12-28 09:01:44 +0000

    autocomplete for CI_Migration

--- application/config/phpstorm_autocomplete.php
+++ application/config/phpstorm_autocomplete.php
@@ -128,6 +128,70 @@ class CI_Controller {}
  */
 class CI_Model {}
 
+/**
+ * @property CI_Benchmark        $benchmark                           This class enables you to mark points and calculate the time difference between them. Memory consumption can also be displayed.
+ * @property CI_Calendar         $calendar                            This class enables the creation of calendars
+ * @property CI_Cache            $cache                               Caching Class
+ * @property CI_Cart             $cart                                Shopping Cart Class
+ * @property CI_Config           $config                              This class contains functions that enable config files to be managed
+ * @property CI_Controller       $controller                          This class object is the super class that every library in CodeIgniter will be assigned to
+ * @property CI_DB_forge         $dbforge                             Database Forge Class
+ * @property CI_DB_query_builder $db                                  This is the platform-independent base Query Builder implementation class
+ * @property CI_DB_utility       $dbutil                              Database Utility Class
+ * @property CI_Driver_Library   $driver                              Driver Library Class
+ * @property CI_Email            $email                               Permits email to be sent using Mail, Sendmail, or SMTP
+ * @property CI_Encrypt          $encrypt                             Provides two-way keyed encoding using Mcrypt
+ * @property CI_Encryption       $encryption                          Provides two-way keyed encryption via PHP's MCrypt and/or OpenSSL extensions
+ * @property CI_Exceptions       $exceptions                          Exceptions Class
+ * @property MY_Form_validation  $form_validation                     Form Validation Class
+ * @property CI_FTP              $ftp                                 FTP Class
+ * @property CI_Hooks            $hooks                               Provides a mechanism to extend the base system without hacking
+ * @property CI_Image_lib        $image_lib                           Image Manipulation class
+ * @property CI_Input            $input                               Pre-processes global input data for security
+ * @property CI_Javascript       $javascript                          Javascript Class
+ * @property CI_Jquery           $jquery                              Jquery Class
+ * @property CI_Lang             $lang                                Language Class
+ * @property CI_Loader           $load                                Loads framework components
+ * @property CI_Log              $log                                 Logging Class
+ * @property CI_Migration        $migration                           All migrations should implement this, forces up() and down() and gives access to the CI super-global
+ * @property CI_Model            $model                               CodeIgniter Model Class
+ * @property MY_Output           $output                              Responsible for sending final output to the browser
+ * @property CI_Pagination       $pagination                          Pagination Class
+ * @property CI_Parser           $parser                              Parser Class
+ * @property CI_Profiler         $profiler                            This class enables you to display benchmark, query, and other data in order to help with debugging and optimization.
+ * @property CI_Router           $router                              Parses URIs and determines routing
+ * @property CI_Security         $security                            Security Class
+ * @property CI_Session          $session                             Session Class
+ * @property CI_Table            $table                               Lets you create tables manually or from database result objects, or arrays
+ * @property CI_Trackback        $trackback                           Trackback Sending/Receiving Class
+ * @property CI_Typography       $typography                          Typography Class
+ * @property CI_Unit_test        $unit                                Simple testing class
+ * @property CI_Upload           $upload                              File Uploading Class
+ * @property CI_URI              $uri                                 Parses URIs and determines routing
+ * @property CI_User_agent       $agent                               Identifies the platform, browser, robot, or mobile device of the browsing agent
+ * @property CI_Xmlrpc           $xmlrpc                              XML-RPC request handler class
+ * @property CI_Xmlrpcs          $xmlrpcs                             XML-RPC server class
+ * @property CI_Zip              $zip                                 Zip Compression Class
+ * @property CI_Utf8             $utf8                                Provides support for UTF-8 environments
+ *
+ * @property MY_Controller      $my_controller
+ * @property Auth_Controller    $auth_controller
+ *
+ * @property Bcrypt             $bcrypt
+ * @property Ion_auth           $ion_auth1
+ * @property Recaptcha          $recaptcha
+ * @property Gravatar           $gravatar
+ * @property Limiter            $limiter
+ *
+ * @property User_model         $User
+ * @property User_Options_model $User_Options
+ * @property Auth_model         $Auth
+ * @property Ion_auth_model     $ion_auth
+ * @property Tracker_Model      $Tracker
+ * @property History_Model      $History
+ */
+class CI_Migration {}
+
 
 /**
  * @property CI_Benchmark        $benchmark                           This class enables you to mark points and calculate the time difference between them. Memory consumption can also be displayed.
