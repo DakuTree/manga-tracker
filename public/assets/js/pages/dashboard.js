16823fa1cb9eabe902a2ff9c1ff468c27e4b966a
Daku <admin@codeanimu.net>
2017-08-09 15:25:04 +0100

    make sure filters are reset on reload [ci skip]

--- public/assets/js/pages/dashboard.js
+++ public/assets/js/pages/dashboard.js
@@ -42,12 +42,9 @@ $(function(){
 
 		widgets: ['zebra', 'filter'],
 		widgetOptions : {
-			// filter_anyMatch replaced! Instead use the filter_external option
-			// Set to use a jQuery selector (or jQuery object) pointing to the
-			// external filter (column specific or any match)
 			filter_external : '#search',
 			filter_columnFilters: false,
-			filter_saveFilters : true,
+			filter_saveFilters : false,
 			filter_reset: '.reset'
 		}
 	});
