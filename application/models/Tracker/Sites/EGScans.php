4cce5cb3dbd2a0d0cbb753f8bb02d14ca9eb20cb
Daku <admin@codeanimu.net>
2017-08-19 12:31:25 +0100

    Extra can be uppercase | fixes #175 [ci skip]

--- application/models/Tracker/Sites/EGScans.php
+++ application/models/Tracker/Sites/EGScans.php
@@ -2,7 +2,7 @@
 
 class EGScans extends Base_Site_Model {
 	public $titleFormat   = '/^[A-Za-z0-9\-_\!,]+$/';
-	public $chapterFormat = '/^Chapter_[0-9]+(?:_extra)?$/';
+	public $chapterFormat = '/^Chapter_[0-9]+(?:_[E|e]xtra)?$/';
 
 	public function getFullTitleURL(string $title_url) : string {
 		return "http://read.egscans.com/{$title_url}/";
