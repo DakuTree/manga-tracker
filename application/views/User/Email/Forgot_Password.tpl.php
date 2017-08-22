f421e339bdd42658485740ec5cccfb8282b117c3
Daku <admin@codeanimu.net>
2017-08-22 12:43:49 +0100

    Reset password email now mentions link expiry time. [ci skip]

--- application/views/User/Email/Forgot_Password.tpl.php
+++ application/views/User/Email/Forgot_Password.tpl.php
@@ -1,6 +1,10 @@
 <html>
 <body>
-	<p>Please click this link to <a href="<?=base_url("user/reset_password/$forgotten_password_code")?>">Reset Your Password</a>.</p>
+	<p>
+		Please click this link to <a href="<?=base_url("user/reset_password/$forgotten_password_code")?>">Reset Your Password</a>.
+		<br/>
+		This link will expire in 12 hours, or after you reset your password.
+	</p>
 	<br/>
 	<p><i>This email was sent from a notification-only address that can't accept incoming email. Please don't reply to this message.</i></p>
 </body>
