a32e457e8cb21470c964a2d8b06a0f37ee27d71c
Daku <admin@codeanimu.net>
2017-08-30 17:24:49 +0100

    Champion Scans doesn't use https

--- application/models/Tracker/Sites/ChampionScans.php
+++ application/models/Tracker/Sites/ChampionScans.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1); defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ChampionScans extends Base_FoolSlide_Site_Model {
-	public $baseURL = 'https://reader.championscans.com';
+	public $baseURL = 'http://reader.championscans.com';
 }
