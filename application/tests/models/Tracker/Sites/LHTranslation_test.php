75e723299319f845c8333e6427e340934b1c1dd7
Daku <admin@codeanimu.net>
2017-08-30 17:24:05 +0100

    Tests for LHTranslation | closes #191

---
+++ application/tests/models/Tracker/Sites/LHTranslation_test.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @coversDefaultClass LHTranslation
+ */
+class LHTranslation_test extends SiteTestCase {
+	public function test_success() {
+		$testSeries = [
+			'tondemo-skill-de-isekai-hourou-meshi'                        => 'Tondemo Skill de Isekai Hourou Meshi',
+			'isekai-goumon-hime'                                          => 'Isekai Goumon Hime',
+			'magi-craft-meister'                                          => 'Magi Craft Meister',
+			'isekai-shihai-no-skill-taker-zero-kara-hajimeru-dorei-harem' => 'Isekai Shihai no Skill Taker: Zero kara Hajimeru Dorei Harem',
+			'grancrest-senki'                                             => 'Grancrest Senki'
+		];
+		$this->_testSiteSuccessRandom($testSeries);
+	}
+	public function test_failure() {
+		$this->_testSiteFailure('Bad Status Code (302)');
+	}
+}
