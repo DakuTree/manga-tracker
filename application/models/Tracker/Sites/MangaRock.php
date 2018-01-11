f282715be5954478784a75dbd7167586a2a7a57f
Angus Johnston <dakutree@codeanimu.net>
2018-01-11 13:42:56 +0000

    missing slash in chapter format | #240 [ci skip]

--- application/models/Tracker/Sites/MangaRock.php
+++ application/models/Tracker/Sites/MangaRock.php
@@ -2,7 +2,7 @@
 
 class MangaRock extends Base_Site_Model {
 	public $titleFormat   = '/^[0-9]+$/';
-	public $chapterFormat = '/^[0-9]+:--:(?:v[0-9]+)?c[0-9\.]+$/';
+	public $chapterFormat = '/^[0-9]+:--:(?:v[0-9]+\/)?c[0-9\.]+$/';
 
 	public function getFullTitleURL(string $title_url) : string {
 		return "https://mangarock.com/manga/mrs-serie-{$title_url}/";
