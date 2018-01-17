a4f8f334eb170a294cb54541b84ac48a0a6abe36
Angus Johnston <dakutree@codeanimu.net>
2018-01-17 18:53:02 +0000

    forgot to update meta [ci skip]

--- public/userscripts/manga-tracker.meta.js
+++ public/userscripts/manga-tracker.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Manga Tracker
 // @namespace    https://github.com/DakuTree/userscripts
-// @version      1.8.47
+// @version      1.8.48
 // @downloadURL  https://trackr.moe/userscripts/manga-tracker.user.js
 // @updateURL    https://trackr.moe/userscripts/manga-tracker.meta.js
 // ==/UserScript==
