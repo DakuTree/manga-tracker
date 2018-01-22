69afa95bd9a84361d162115fa2e7d56c88ddd717
Angus Johnston <dakutree@codeanimu.net>
2018-01-22 20:57:39 +0000

    this route no longer exists [ci skip]

--- application/config/config.php
+++ application/config/config.php
@@ -448,7 +448,6 @@ $config['csrf_expire']       = 7200;
 $config['csrf_regenerate']   = TRUE;
 $config['csrf_exclude_uris'] = array(
 	'ajax/userscript/update',
-	'ajax/userscript/report_issue',
 	'ajax/userscript/favourite',
 
 	//TODO: These <should> use CSRF, but for now we're going to just exclude them.
