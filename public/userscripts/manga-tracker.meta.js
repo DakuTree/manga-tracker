adff2ccc949f582459ebb479ab243d1406b4003f
Angus Johnston <dakutree@codeanimu.net>
2018-01-28 09:41:25 +0000

    bump versions [ci skip]

--- public/userscripts/manga-tracker.meta.js
+++ public/userscripts/manga-tracker.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Manga Tracker
 // @namespace    https://github.com/DakuTree/userscripts
-// @version      1.9.17
+// @version      1.9.18
 // @downloadURL  https://trackr.moe/userscripts/manga-tracker.user.js
 // @updateURL    https://trackr.moe/userscripts/manga-tracker.meta.js
 // ==/UserScript==
--- public/userscripts/manga-tracker.user.js
+++ public/userscripts/manga-tracker.user.js
@@ -69,8 +69,8 @@
 // @include      /^http:\/\/riceballicious\.info\/fs\/read\/.*?\/[a-z]+\/[0-9]+\/[0-9]+(\/.*)?$/
 // @include      /^https:\/\/mangadex\.com\/chapter\/[0-9]+(?:\/[0-9]+)?$/
 // @include      /^https?:\/\/reader\.tukimoop\.pw\/read\/.*?\/[a-z]+\/[0-9]+\/[0-9]+(\/.*)?$/
-// @updated      2018-01-25
-// @version      1.9.17
+// @updated      2018-01-28
+// @version      1.9.18
 // @downloadURL  https://trackr.moe/userscripts/manga-tracker.user.js
 // @updateURL    https://trackr.moe/userscripts/manga-tracker.meta.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
@@ -105,7 +105,7 @@
 // @require      https://trackr.moe/userscripts/sites/Lolitannia.js
 // @require      https://trackr.moe/userscripts/sites/LOLScans.js
 // @require      https://trackr.moe/userscripts/sites/MangaCow.2.js
-// @require      https://trackr.moe/userscripts/sites/MangaDex.6.js
+// @require      https://trackr.moe/userscripts/sites/MangaDex.7.js
 // @require      https://trackr.moe/userscripts/sites/MangaFox.2.js
 // @require      https://trackr.moe/userscripts/sites/MangaHere.3.js
 // @require      https://trackr.moe/userscripts/sites/MangaichiScans.js
