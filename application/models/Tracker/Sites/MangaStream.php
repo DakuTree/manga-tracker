81ef31283816eefd51914427e42b63fb198fc882
Angus Johnston <dakutree@codeanimu.net>
2018-01-30 16:39:46 +0000

    Custom Updater support for MangaStream

--- application/models/Tracker/Sites/MangaStream.php
+++ application/models/Tracker/Sites/MangaStream.php
@@ -40,4 +40,73 @@ class MangaStream extends Base_Site_Model {
 
 		return (!empty($titleData) ? $titleData : NULL);
 	}
+
+	public function doCustomUpdate() {
+		$titleDataList = [];
+
+		$updateURL = "https://readms.net/manga";
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
+
+			$data = preg_replace('/^[\s\S]+<ul class="new-list">/', '<ul class="new-list">', $data);
+			$data = preg_replace('/<\/ul>[\s\S]+$/', '</ul>', $data);
+
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
+
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//ul[@class='new-list']/li");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
+
+					$nodes_title   = $xpath->query("a/text()", $row);
+					$nodes_chapter = $xpath->query("a", $row);
+					$nodes_latest  = $xpath->query("a/span[@class='pull-right']", $row);
+
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title   = $nodes_title->item(0);
+						$chapter = $nodes_chapter->item(0);
+
+						preg_match('/^\/r\/(?<url>[a-z0-9_]+)\/(?<chapter>.*?\/[0-9]+)/', $chapter->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'];
+
+						if(!array_key_exists($title_url, $titleDataList)) {
+							$titleData['title'] = trim($title->textContent);
+
+							$titleData['latest_chapter'] = $title_url_arr['chapter'];
+
+							$dateString = trim($nodes_latest->item(0)->textContent);
+							switch($dateString) {
+								case 'Today':
+									$dateString = date("Y-m-d", now());
+									break;
+
+								case 'Yesterday':
+									$dateString = date("Y-m-d", strtotime("-1 days"));
+									break;
+
+								default:
+									//Do nothing
+									break;
+							}
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+							$titleDataList[$title_url] = $titleData;
+						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+					}
+				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
+			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
+		}
+
+		return $titleDataList;
+	}
 }
