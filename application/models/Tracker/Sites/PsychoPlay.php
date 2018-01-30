d17aabbd0b14ebc5845141bb45578b6b44fd8a26
Angus Johnston <dakutree@codeanimu.net>
2018-01-30 20:11:04 +0000

    Custom Updater support for PsychoPlay

--- application/models/Tracker/Sites/PsychoPlay.php
+++ application/models/Tracker/Sites/PsychoPlay.php
@@ -33,4 +33,59 @@ class PsychoPlay extends Base_Site_Model {
 		}
 		return (!empty($titleData) ? $titleData : NULL);
 	}
+
+
+	public function doCustomUpdate() {
+		$titleDataList = [];
+
+		$updateURL = "https://psychoplay.co/latest";
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
+
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
+
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//div[@class='content-wrapper']/div[@class='row']/div/div");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
+
+					$nodes_title   = $xpath->query("div[@class='caption']/h6/a", $row);
+					$nodes_chapter = $xpath->query("div[@class='panel-footer no-padding']/a", $row);
+					$nodes_latest  = $xpath->query("div[@class='caption']/text()", $row);
+
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title = $nodes_title->item(0);
+
+						preg_match('/(?<url>[^\/]+(?=\/$|$))/', $title->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'];
+
+						if(!array_key_exists($title_url, $titleDataList)) {
+							$titleData['title'] = trim($title->textContent);
+
+							$chapter = $nodes_chapter->item(0);
+							preg_match('/(?<chapter>[^\/]+(?=\/$|$))/', $chapter->getAttribute('href'), $chapter_arr);
+							$titleData['latest_chapter'] = $chapter_arr['chapter'];
+
+							$dateString = trim(str_replace('Added ', '', $nodes_latest->item(0)->textContent));
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+							$titleDataList[$title_url] = $titleData;
+						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+					}
+				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
+			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
+		}
+
+		return $titleDataList;
+	}
 }
