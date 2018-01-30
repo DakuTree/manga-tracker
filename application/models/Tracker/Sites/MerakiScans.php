87a772415b0500e1816d2f48654ca73aa335549f
Angus Johnston <dakutree@codeanimu.net>
2018-01-30 19:12:46 +0000

    Custom Updater support for MerakiScans

--- application/models/Tracker/Sites/MerakiScans.php
+++ application/models/Tracker/Sites/MerakiScans.php
@@ -41,4 +41,71 @@ class MerakiScans extends Base_Site_Model {
 
 		return (!empty($titleData) ? $titleData : NULL);
 	}
+
+	public function doCustomUpdate() {
+		$titleDataList = [];
+
+		$updateURL = "http://merakiscans.com/";
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
+
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
+
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//div[@class='wpm_pag mng_lts_chp grp']/div[@class='row']/div[@class='det']");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
+
+					$nodes_title   = $xpath->query("a[1]", $row);
+					$nodes_chapter = $xpath->query("ul/li[1]/a", $row);
+					$nodes_latest  = $xpath->query("ul/li[1]/a/b[@class='dte']", $row);
+
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title   = $nodes_title->item(0);
+						$chapter = $nodes_chapter->item(0);
+
+						preg_match('/(?<url>[^\/]+(?=\/$|$))/', $title->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'];
+
+						if(!array_key_exists($title_url, $titleDataList)) {
+							$titleData['title'] = trim($title->getAttribute('title'));
+
+							preg_match('/(?<chapter>[^\/]+(?=\/$|$))/', $chapter->getAttribute('href'), $chapter_arr);
+							$titleData['latest_chapter'] = $chapter_arr['chapter'];
+
+							$dateString = trim(str_replace('Published on ', '', $nodes_latest->item(0)->textContent));
+							switch($dateString) {
+								case 'Today':
+									$dateString = date("Y-m-d", now());
+									break;
+
+								case 'Yesterday':
+									$dateString = date("Y-m-d", strtotime("-1 days"));
+									break;
+
+								default:
+									//Do nothing
+									break;
+							}
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+							$titleDataList[$title_url] = $titleData;
+						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+					}
+				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
+			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
+		}
+
+		return $titleDataList;
+	}
 }
