e3dbb00107693195b02282d1a33291b934a7abda
Angus Johnston <dakutree@codeanimu.net>
2018-01-31 09:59:46 +0000

    Custom Updater support for MangaKakalot | closes #307 [ci skip]

--- application/models/Tracker/Sites/MangaKakalot.php
+++ application/models/Tracker/Sites/MangaKakalot.php
@@ -4,6 +4,8 @@ class MangaKakalot extends Base_Site_Model {
 	public $titleFormat   = '/^[a-zA-Z_\-0-9]+$/';
 	public $chapterFormat = '/^[0-9\.]+$/';
 
+	public $customType    = 2;
+
 	public function getFullTitleURL(string $title_url) : string {
 		return "http://mangakakalot.com/manga/{$title_url}/";
 	}
@@ -41,5 +43,63 @@ class MangaKakalot extends Base_Site_Model {
 		}
 
 		return (!empty($titleData) ? $titleData : NULL);
+	}
+
+	public function doCustomUpdate() {
+		$titleDataList = [];
+
+		//FIXME: latest list does not work as no timestamp, use  http://mangakakalot.com/
+		$updateURL = "http://mangakakalot.com";
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
+
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
+
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//div[@class='doreamon']/div[@class='itemupdate first']/ul");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
+
+					$nodes_title   = $xpath->query("li[1]/h3/a[1]", $row);
+					$nodes_chapter = $xpath->query("li[2]/span/a", $row);
+					$nodes_latest  = $xpath->query("li[2]/i", $row);
+
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title = $nodes_title->item(0);
+
+						preg_match('/(?<url>[^\/]+(?=\/$|$))/', $title->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'];
+
+						if(!array_key_exists($title_url, $titleDataList)) {
+							$titleData['title'] = trim($title->textContent);
+
+							$chapter = $nodes_chapter->item(0);
+							preg_match('/(?<chapter>[^\/]+(?=\/$|$))/', $chapter->getAttribute('href'), $chapter_arr);
+							$titleData['latest_chapter'] = str_replace('chapter_', '', $chapter_arr['chapter']);
+
+							$dateString = trim($nodes_latest->item(0)->textContent);
+							if(strpos($dateString, 'ago') === FALSE) {
+								$dateString = date('Y-').$dateString;
+							}
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+							$titleDataList[$title_url] = $titleData;
+						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+					}
+				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
+			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
+		}
+
+		return $titleDataList;
 	}
 }
