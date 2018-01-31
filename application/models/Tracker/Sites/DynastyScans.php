8eae8644a4265ae51c43773c99aa5b8eee0c4bdd
Angus Johnston <dakutree@codeanimu.net>
2018-01-31 11:41:49 +0000

    Custom Updater support for DynastyScans | closes #302

--- application/models/Tracker/Sites/DynastyScans.php
+++ application/models/Tracker/Sites/DynastyScans.php
@@ -6,6 +6,8 @@ class DynastyScans extends Base_Site_Model {
 	public $titleFormat   = '/^[a-z0-9_]+:--:(?:0|1)$/';
 	public $chapterFormat = '/^[0-9a-z_]+$/';
 
+	public $customType    = 2;
+
 	public function getFullTitleURL(string $title_url) : string {
 		$title_parts = explode(':--:', $title_url);
 		$url_type = ($title_parts[1] == '0' ? 'series' : 'chapters');
@@ -110,5 +112,63 @@ class DynastyScans extends Base_Site_Model {
 				break;
 		}
 		return (!empty($titleData) ? $titleData : NULL);
+	}
+
+	public function doCustomUpdate() {
+		$titleDataList = [];
+
+		$updateURL = "https://dynasty-scans.com/chapters/added";
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
+
+			$data = preg_replace('/^[\s\S]+<dl class="chapter-list">/', '<dl class="chapter-list">', $data);
+			$data = preg_replace('/<\/dl>[\s\S]+$/', '</dl>', $data);
+
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
+
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//dl[@class='chapter-list']/dt/following::dd[count(preceding::dt) = 1]");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
+
+					$nodes_title   = $xpath->query("a[1]", $row);
+					$nodes_chapter = $xpath->query("a[1]", $row);
+					$nodes_latest  = $xpath->query("small[last()]", $row);
+
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title = $nodes_title->item(0);
+
+						preg_match('/^\/chapters\/(?<url>.*?)(?:_(?<chapter>c[^_]+?))?$/', $title->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'] . ':--:' . ((int) empty($title_url_arr['chapter']));
+
+						if(!array_key_exists($title_url, $titleDataList)) {
+							$titleData['title'] = trim($title->textContent);
+							if(!empty($title_url_arr['chapter'])) {
+								$titleData['title'] = substr($titleData['title'], 0, stripos($titleData['title'], ' '.$title_url_arr['chapter']));
+							}
+
+							$titleData['latest_chapter'] = $title_url_arr['chapter'] ?? 'oneshot';
+
+							$dateString = trim(str_replace('\'', '', str_replace('released ', '', $nodes_latest->item(0)->textContent)));
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+							$titleDataList[$title_url] = $titleData;
+						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+					}
+				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
+			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
+		}
+
+		return $titleDataList;
 	}
 }
