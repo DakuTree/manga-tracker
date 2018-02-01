c84fe31d3d21df4fb2ab5d778c1006c0daed0384
Angus Johnston <dakutree@codeanimu.net>
2018-02-01 15:47:51 +0000

    phpStorm watcher script to allow dev userscript

---
+++ _scripts/userscript-devtoprod.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+##### MODULES  #####{
+use File::Basename;
+use Cwd qw(abs_path);
+
+use File::Slurp;
+
+use feature 'say';
+
+use strict;
+use warnings;
+# use diagnostics;
+####################}
+
+if($> != 0) { die("Script must be run as root!"); }
+
+my $trackrLocation = dirname(abs_path(__FILE__)) =~ s/\/_scripts$//r;
+
+my $userscriptDev  = read_file($trackrLocation."/public/userscripts/manga-tracker.dev.user.js");
+my $userscriptProd = ($userscriptDev =~ s/dev\.trackr\.moe/trackr\.moe/rg);
+
+my $goodReplace  = "=== 'https://dev.trackr.moe'";
+my $badReplace = "=== 'https://trackr.moe'";
+$userscriptProd =~ s/$badReplace/$goodReplace/g;
+
+overwrite_file($trackrLocation."/public/userscripts/manga-tracker.user.js", {binmode => ':utf8'}, $userscriptProd);
