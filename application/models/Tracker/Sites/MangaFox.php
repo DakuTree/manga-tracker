79535d66c41bc3f9961702adc3d4f018d44a0c4d
Angus Johnston <dakutree@codeanimu.net>
2018-02-03 12:07:13 +0000

    MangaFox | Custom Updater now using latest releases, instead of following page

--- application/models/Tracker/Sites/MangaFox.php
+++ application/models/Tracker/Sites/MangaFox.php
@@ -3,7 +3,7 @@
 class MangaFox extends Base_Site_Model {
 	public $titleFormat   = '/^[a-z0-9_]+$/';
 	public $chapterFormat = '/^(?:v[0-9a-zA-Z]+\/)?c[0-9\.]+$/';
-	public $customType    = 1;
+	public $customType    = 2;
 
 	public function getFullTitleURL(string $title_url) : string {
 		return "http://fanfox.net/manga/{$title_url}/";
@@ -46,90 +46,70 @@ class MangaFox extends Base_Site_Model {
 		return (!empty($titleData) ? $titleData : NULL);
 	}
 
-	//FIXME: This entire thing feels like an awful implementation....BUT IT WORKS FOR NOW.
-	public function handleCustomFollow(callable $callback, string $data = "", array $extra = []) {
-		preg_match('/var sid=(?<id>[0-9]+);/', $data, $matches);
-
-		$formData = [
-			'action' => 'add',
-			'sid'    => $matches['id']
-		];
-
-		$cookies = [
-			"mfvb_userid={$this->config->item('mangafox_userid')}",
-			"mfvb_password={$this->config->item('mangafox_password')}",
-			"bmsort=last_chapter"
-		];
-		$content = $this->get_content('http://fanfox.net/ajax/bookmark.php', implode("; ", $cookies), "", TRUE, TRUE, $formData);
-
-		$callback($content, $matches['id'], function($body) {
-			return $body == 'true';
-		});
-	}
 	public function doCustomUpdate() {
 		$titleDataList = [];
 
-		$cookies = [
-			"mfvb_uf={$this->config->item('mangafox_userid')}",
-			"mfvb_pf={$this->config->item('mangafox_password')}",
-			"bmsort=last_chapter",
-			"bmorder=za"
-		];
-		$content = $this->get_content('http://fanfox.net/bookmark/?status=currentreading&sort=last_chapter&order=za', implode("; ", $cookies), "", TRUE);
+		$updateURL = 'http://fanfox.net/releases/';
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+			$data = $content['body'];
 
-		if(!is_array($content)) {
-			log_message('error', "{$this->site} /bookmark | Failed to grab URL (See above curl error)");
-		} else {
-			$headers     = $content['headers'];
-			$status_code = $content['status_code'];
-			$data        = $content['body'];
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(TRUE);
+			$dom->loadHTML($data);
+			libxml_use_internal_errors(FALSE);
 
-			if(!($status_code >= 200 && $status_code < 300)) {
-				log_message('error', "{$this->site} /bookmark | Bad Status Code ({$status_code})");
-			} else if(empty($data)) {
-				log_message('error', "{$this->site} /bookmark | Data is empty? (Status code: {$status_code})");
-			} else {
-				$data = preg_replace('/^[\s\S]+<ul id="bmlist">/', '<ul id="bmlist">', $data);
-				$data = preg_replace('/<!-- end of bookmark -->[\s\S]+$/', '<!-- end of bookmark -->', $data);
+			$xpath      = new DOMXPath($dom);
+			$nodes_rows = $xpath->query("//ul[@id='updates']/li/div");
+			if($nodes_rows->length > 0) {
+				foreach($nodes_rows as $row) {
+					$titleData = [];
 
-				$dom = new DOMDocument();
-				libxml_use_internal_errors(TRUE);
-				$dom->loadHTML($data);
-				libxml_use_internal_errors(FALSE);
+					$nodes_title   = $xpath->query("h3[@class='title']/a", $row);
+					$nodes_chapter = $xpath->query("dl/dt[1]/span/a", $row);
+					$nodes_latest  = $xpath->query("dl/dt[1]/em", $row);
 
-				$xpath      = new DOMXPath($dom);
-				$nodes_rows = $xpath->query("//ul[@id='bmlist']/li/div[@class='series_grp' and h2[@class='title']/span[@class='updatedch'] and dl]");
-				if($nodes_rows->length > 0) {
-					foreach($nodes_rows as $row) {
-						$titleData = [];
+					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+						$title   = $nodes_title->item(0);
+						$chapter = $nodes_chapter->item(0);
 
-						$nodes_title   = $xpath->query("h2[@class='title']/a[contains(@class, 'title')]", $row);
-						$nodes_chapter = $xpath->query("dl/dt[1]/a[@class='chapter']", $row);
-						$nodes_latest  = $xpath->query("dl/dt[1]/em/span[@class='timing']", $row);
+						preg_match('/(?<url>[^\/]+(?=\/$|$))/', $title->getAttribute('href'), $title_url_arr);
+						$title_url = $title_url_arr['url'];
 
-						if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
-							$title = $nodes_title->item(0);
-
+						if(!array_key_exists($title_url, $titleDataList)) {
 							$titleData['title'] = trim($title->textContent);
 
-
-							$link = preg_replace('/^(.*\/)(?:[0-9]+\.html)?$/', '$1', (string) $nodes_chapter->item(0)->getAttribute('href'));
-							$chapterURLSegments = explode('/', $link);
+							$chapterURLSegments = explode('/', str_replace('/1.html', '', $chapter->getAttribute('href')));
 							$titleData['latest_chapter'] = $chapterURLSegments[5] . (isset($chapterURLSegments[6]) && !empty($chapterURLSegments[6]) ? "/{$chapterURLSegments[6]}" : "");
 
-							$titleData['last_updated'] =  date("Y-m-d H:i:s", strtotime((string) $nodes_latest->item(0)->nodeValue));
+							$dateString = trim($nodes_latest->item(0)->textContent);
+							switch($dateString) {
+								case 'Today':
+									$dateString = date("Y-m-d", now());
+									break;
 
-							$title_url = explode('/', $title->getAttribute('href'))[4];
+								case 'Yesterday':
+									$dateString = date("Y-m-d", strtotime("-1 days"));
+									break;
+
+								default:
+									//Do nothing
+									break;
+							}
+							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
 							$titleDataList[$title_url] = $titleData;
-						} else {
-							log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
 						}
+					} else {
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
 					}
-				} else {
-					log_message('error', "{$this->site} | Following list is empty?");
 				}
+			} else {
+				log_message('error', "{$this->site} | Following list is empty?");
 			}
+		} else {
+			log_message('error', "{$this->site} - Custom updating failed.");
 		}
+
 		return $titleDataList;
 	}
 	public function doCustomCheck(string $oldChapterString, string $newChapterString) : bool {
