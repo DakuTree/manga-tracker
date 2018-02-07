5724d529eb1554c4de00f39ffa3cf885af88bbef
Angus Johnston <dakutree@codeanimu.net>
2018-02-07 13:32:44 +0000

    fix travis issues

---
+++ application/config/production/monolog.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* GENERAL OPTIONS */
+$config['handlers'] = array('file', 'papertrail'); // valid handlers are ci_file | file | new_relic | hipchat | stderr | papertrail
+
+include 'monolog_secure.php';
