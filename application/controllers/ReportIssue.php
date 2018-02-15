d5ee29650531c3ba58fe6381697bcc30a30b589c
Angus Johnston <dakutree@codeanimu.net>
2018-02-15 23:25:59 +0000

    don't completely die if bot detected

--- application/controllers/ReportIssue.php
+++ application/controllers/ReportIssue.php
@@ -14,25 +14,29 @@ class ReportIssue extends MY_Controller {
 		$this->form_validation->set_rules('issue_description', 'Description', 'required|max_length[255]');
 		$this->form_validation->set_rules('issue_url',         'URL',         'valid_url');
 
-		if(!empty($this->input->post('website'))) {
-			log_message('warning',"Bot attempting to spam report issue form!");
-			die();
-		}
 
 		$this->body_data['issue_submitted'] = FALSE;
 		if($isValid = $this->form_validation->run() === TRUE) {
 			//send report
-			$this->body_data['issue_submitted'] = $this->Tracker->issue->report("USERID:" . $this->User->id . " ||| " . $this->input->post('issue_description'), NULL, $this->input->post('issue_url'));
+
+			if(!empty($this->input->post('website'))) {
+				$this->body_data['issue_submitted'] = FALSE;
+				log_message('error',"Bot attempting to spam report issue form: \"".$this->input->post('issue_description')."\"");
+			} else {
+				$this->body_data['issue_submitted'] = $this->Tracker->issue->report("USERID:" . $this->User->id . " ||| " . $this->input->post('issue_description'), NULL, $this->input->post('issue_url'));
+			}
 		}
 
+		$this->body_data['form_url'] = array(
+			'name'        => 'issue_url',
+
+			'class'       => 'form-control',
+
+			'value'       => $this->form_validation->set_value('issue_url') ?: $this->input->get('url', TRUE) ?: ''
+		);
+
 		if(!$isValid) {
-			$this->body_data['form_url'] = array(
-				'name'        => 'issue_url',
-
-				'class'       => 'form-control',
-
-				'value'       => $this->form_validation->set_value('issue_url') ?: $this->input->get('url', TRUE) ?: ''
-			);
+			$this->body_data['form_url']['value'] = '';
 		}
 		$this->_render_page("ReportIssue");
 	}
