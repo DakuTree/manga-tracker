a25e35a95f0538b1df8ce58d94ee26fdab9f3e6d
Angus Johnston <dakutree@codeanimu.net>
2018-02-15 12:51:17 +0000

    MangaDex | Custom Updater now supports multi-lang

--- application/models/Tracker/Sites/MangaDex.php
+++ application/models/Tracker/Sites/MangaDex.php
@@ -36,7 +36,7 @@ class MangaDex extends Base_Site_Model {
 			$content,
 			$title_url,
 			"//h3[contains(@class, 'panel-title')]/text()[1]",
-			"//div[@id='chapters']/div/table/tbody/tr[.//*[@alt='{$title_parts[1]}']][1]", //FIXME: This forces English for now.
+			"//div[@id='chapters']/div/table/tbody/tr[.//*[@alt='{$title_parts[1]}']][1]",
 			"td[6]",
 			"td[1]/a",
 			"Warning: Manga #",
@@ -61,7 +61,7 @@ class MangaDex extends Base_Site_Model {
 	public function doCustomUpdate() {
 		$titleDataList = [];
 
-		$updateURL = "https://mangadex.com/1"; //English only.
+		$updateURL = "https://mangadex.com/0"; //All Languages
 		if(($content = $this->get_content($updateURL, $this->cookieString)) && $content['status_code'] == 200) {
 			$data = $content['body'];
 
@@ -73,35 +73,49 @@ class MangaDex extends Base_Site_Model {
 			$xpath      = new DOMXPath($dom);
 			$nodes_rows = $xpath->query("//div[@class='col-sm-9']/div/table/tbody/tr[.//td[@rowspan]]");
 			if($nodes_rows->length > 0) {
+				$i = 0;
 				foreach($nodes_rows as $row) {
+					$i++;
 					$titleData = [];
 
-					$nodes_title   = $xpath->query("td[3]/a", $row);
-					$nodes_chapter = $xpath->query("following-sibling::tr[1]/td[2]/a", $row);
-					$nodes_latest  = $xpath->query("following-sibling::tr[1]/td[5]/time", $row);
+					$nodes_title         = $xpath->query("td[3]/a", $row);
+					$nodes_rows_chapters = $xpath->query("following-sibling::tr[.//td[@title] and count(preceding-sibling::tr[.//td[@rowspan]])=$i]", $row);
 
-					if($nodes_title->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+					if($nodes_title->length === 1 && $nodes_rows_chapters->length >= 1) {
 						$title = $nodes_title->item(0);
 
 						preg_match('/(?<url>[^\/]+(?=\/$|$))/', $title->getAttribute('href'), $title_url_arr);
-						$title_url = $title_url_arr['url'];
+						$titleID = $title_url_arr['url'];
 
-						if(!array_key_exists($title_url, $titleDataList)) {
-							$titleData['title'] = trim($title->textContent);
+						foreach($nodes_rows_chapters as $rowC) {
+							$nodes_lang     = $xpath->query('td[3]/img', $rowC);
+							$nodes_chapter  = $xpath->query("td[2]/a", $rowC);
+							$nodes_latest   = $xpath->query("td[5]/time", $rowC);
 
-							$chapter = $nodes_chapter->item(0);
-							$chapterID     = explode('/', (string) $chapter->getAttribute('href'))[2];
-							$chapterNumber = preg_replace('/v\//', '', preg_replace('/^(?:Vol(?:ume|\.) ([0-9\.]+)?.*?)?Ch(?:apter|\.) ([0-9\.v]+)[\s\S]*$/', 'v$1/c$2', trim((string) $chapter->textContent)));
+							if($nodes_lang->length === 1 && $nodes_chapter->length === 1 && $nodes_latest->length === 1) {
+								$lang = $nodes_lang->item(0)->getAttribute('title');
 
-							$titleData['latest_chapter'] = $chapterID . ':--:' . $chapterNumber;
+								$title_url = $titleID . ':--:'. $lang;
+								if(!array_key_exists($title_url, $titleDataList)) {
+									$titleData['title'] = trim($title->textContent);
 
-							$dateString = trim($nodes_latest->item(0)->getAttribute('datetime'));
-							$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+									$chapter = $nodes_chapter->item(0);
+									$chapterID     = explode('/', (string) $chapter->getAttribute('href'))[2];
+									$chapterNumber = preg_replace('/v\//', '', preg_replace('/^(?:Vol(?:ume|\.) ([0-9\.]+)?.*?)?Ch(?:apter|\.) ([0-9\.v]+)[\s\S]*$/', 'v$1/c$2', trim((string) $chapter->textContent)));
 
-							$titleDataList[$title_url] = $titleData;
+									$titleData['latest_chapter'] = $chapterID . ':--:' . $chapterNumber;
+
+									$dateString = trim($nodes_latest->item(0)->getAttribute('datetime'));
+									$titleData['last_updated'] = date("Y-m-d H:i:s", strtotime($dateString));
+
+									$titleDataList[$title_url] = $titleData;
+								}
+							} else {
+								log_message('error', "{$this->site}/Custom | Invalid amount of nodes (LANG: {$nodes_lang->length} | CHAPTER: {$nodes_chapter->length} | LATEST: {$nodes_latest->length})");
+							}
 						}
 					} else {
-						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTER: {$nodes_chapter->length}) | LATEST: {$nodes_latest->length})");
+						log_message('error', "{$this->site}/Custom | Invalid amount of nodes (TITLE: {$nodes_title->length} | CHAPTERS: {$nodes_rows_chapters->length})");
 					}
 				}
 			} else {
