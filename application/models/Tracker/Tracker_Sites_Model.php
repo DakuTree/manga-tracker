6f3cbf5e10b3f2fe07549b22682718529f7e8e75
Angus Johnston <dakutree@codeanimu.net>
2018-04-09 09:47:16 +0100

    this should fail if image wasn't found, not if it was

--- application/models/Tracker/Tracker_Sites_Model.php
+++ application/models/Tracker/Tracker_Sites_Model.php
@@ -786,7 +786,7 @@ abstract class Base_GlossyBright_Site_Model extends Base_Site_Model {
 			'pubdate',
 			'title',
 			function($data) {
-				return strpos($data, '<image>') !== FALSE;
+				return strpos($data, '<image>') === FALSE;
 			}
 		);
 		if($data) {
