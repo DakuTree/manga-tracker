87e6af06947a9d619c2747963c75fd950ece5420
Angus Johnston <dakutree@codeanimu.net>
2018-06-16 22:51:18 +0100

    use localhost subdomain to avoid conflicts | #357

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -5,8 +5,9 @@
 | Base Site URL
 |--------------------------------------------------------------------------
 */
-$config['base_url']   = 'http://localhost:20180';
-$config['static_url'] = 'http://static.localhost:20180';
+//NOTE: We can't just use 'localhost' as some browsers block cookies on localhost
+$config['base_url']   = 'http://manga-tracker.localhost:20180';
+$config['static_url'] = 'http://static.manga-tracker.localhost:20180';
 
 /*
 |--------------------------------------------------------------------------
