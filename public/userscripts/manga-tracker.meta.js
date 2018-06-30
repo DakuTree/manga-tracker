833e24e7bf3125b170fd07676392a5029b38d828
Angus Johnston <dakutree@codeanimu.net>
2018-06-30 09:32:11 +0100

    alt elpsycongroo domain

--- public/userscripts/manga-tracker.meta.js
+++ public/userscripts/manga-tracker.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Manga Tracker
 // @namespace    https://github.com/DakuTree/userscripts
-// @version      1.10.44
+// @version      1.10.45
 // @downloadURL  https://trackr.moe/userscripts/manga-tracker.user.js
 // @updateURL    https://trackr.moe/userscripts/manga-tracker.meta.js
 // ==/UserScript==
--- public/userscripts/sites/ElPsyCongroo.js
+++ public/userscripts/sites/ElPsyCongroo.js
@@ -1,5 +1,14 @@
 (function(sites) {
 	/**
+	 * ElPsyCongroo (FoolSlide) (Alt Domain)
+	 * @type {SiteObject}
+	 */
+	sites['elpsycongroo.pw'] = {
+		preInit : function() {
+			location.href = location.href.replace('elpsycongroo.pw', 'elpsycongroo.tk');
+		}
+	};
+	/**
 	 * ElPsyCongroo (FoolSlide)
 	 * @type {SiteObject}
 	 */
