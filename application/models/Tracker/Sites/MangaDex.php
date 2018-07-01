00fbbde0b6c29dc23f3297f462de3e88d0ef87d9
Angus Johnston <dakutree@codeanimu.net>
2018-07-01 20:39:09 +0100

    lower MangaDex rate limit we got caught again, supposedly they went from 600 per minute to 500 per 10 minutes. Great.

--- application/models/Tracker/Sites/MangaDex.php
+++ application/models/Tracker/Sites/MangaDex.php
@@ -11,7 +11,7 @@ class MangaDex extends Base_Site_Model {
 
 	public $cookieString  = 'mangadex_h_toggle=1';
 
-	public $siteRateLimit = 500; //MangaDex limit is 600 in 60s, but to avoid going over by mistake, we go a bit lower.
+	public $siteRateLimit = 400; //MangaDex limit is 500 in 10m, but to avoid going over by mistake, we go a bit lower.
 
 	public function getFullTitleURL(string $title_url) : string {
 		$title_parts = explode(':--:', $title_url);
