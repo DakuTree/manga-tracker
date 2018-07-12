a204416f7f8c082f9c1439a2d2f1f51491f1bee6
Angus Johnston <dakutree@codeanimu.net>
2018-07-12 15:24:45 +0100

    MangaRock - Fix issues with bad chapters

--- application/models/Tracker/Sites/MangaRock.php
+++ application/models/Tracker/Sites/MangaRock.php
@@ -40,10 +40,10 @@ class MangaRock extends Base_Site_Model {
 
 					$latestChapter = end($json['data']['chapters']);
 
-					preg_match('/^(?:Vol\.(?<volume>\S+) )?(?:Chapter (?<chapter>[^\s:]+)(?:\s?-\s?(?<extra>[0-9]+))?):?.*/', $latestChapter['name'], $text);
+					preg_match('/^(?:Vol\.(?<volume>\S+) )?(?:Chapter[ |:]+(?<chapter>[^\s:]+))?(?<extra>.*?)$/', $latestChapter['name'], $text);
 					preg_match('/^mrs-chapter-(?<id>[0-9]+)$/', $latestChapter['oid'], $matches_id);
-					$titleData['latest_chapter'] = $matches_id['id'].':--:'.((!empty($text['volume']) ? 'v'.$text['volume'].'/' : '') . 'c'.$text['chapter'] . (!empty($text['extra']) ? '-'.$text['extra'] : ''));;
-					$titleData['last_updated'] = date("Y-m-d H:i:s", $latestChapter['updatedAt']);
+					$titleData['latest_chapter'] = $matches_id['id'].':--:'.((!empty($text['volume']) ? 'v'.$text['volume'].'/' : '') . ($text['chapter'] === 'Oneshot' ? 'Oneshot' : 'c'.$text['chapter']));
+					$titleData['last_updated'] = date('Y-m-d H:i:s', $latestChapter['updatedAt']);
 				} else {
 					log_message('error', "{$this->site} : {$title_url} | Failure string matched");
 				}
@@ -57,7 +57,7 @@ class MangaRock extends Base_Site_Model {
 		$titleDataList = [];
 
 		$updateURL = 'https://api.mangarockhd.com/query/web400/mrs_latest';
-		if(($content = $this->get_content($updateURL)) && $content['status_code'] == 200) {
+		if(($content = $this->get_content($updateURL)) && $content['status_code'] === 200) {
 			$json = json_decode($content['body'], TRUE);
 			if(!empty($json['data'])) {
 				foreach($json['data'] as $row) {
@@ -69,9 +69,9 @@ class MangaRock extends Base_Site_Model {
 
 						$latestChapter = reset($row['new_chapters']);
 
-						preg_match('/^(?:Vol\.(?<volume>\S+) )?(?:Chapter (?<chapter>[^\s:]+)(?:\s?-\s?(?<extra>[0-9]+))?):?.*/', $latestChapter['name'], $text);
+						preg_match('/^(?:Vol\.(?<volume>\S+) )?(?:Chapter[ |:]+(?<chapter>[^\s:]+))?(?<extra>.*?)$/', $latestChapter['name'], $text);
 						preg_match('/^mrs-chapter-(?<id>[0-9]+)$/', $latestChapter['oid'], $matches_id);
-						$titleData['latest_chapter'] = $matches_id['id'].':--:'.((!empty($text['volume']) ? 'v'.$text['volume'].'/' : '') . 'c'.$text['chapter'] . (!empty($text['extra']) ? '-'.$text['extra'] : ''));
+						$titleData['latest_chapter'] = $matches_id['id'].':--:'.((!empty($text['volume']) ? 'v'.$text['volume'].'/' : '') . ($text['chapter'] === 'Oneshot' ? 'Oneshot' : 'c'.$text['chapter']));
 						$titleData['last_updated'] = date('Y-m-d H:i:s', strtotime($latestChapter['updatedAt']));
 
 						$titleDataList[$title_url] = $titleData;
