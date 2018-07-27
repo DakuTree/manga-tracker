c190d08a2249590593624ffba01702a991ff6c3f
Angus Johnston <dakutree@codeanimu.net>
2018-07-27 10:48:03 +0100

    TrashScanlations | Fix trailing slash causing redirects

--- application/models/Tracker/Sites/TrashScanlations.php
+++ application/models/Tracker/Sites/TrashScanlations.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1); defined('BASEPATH') OR exit('No direct script access allowed');
 
 class TrashScanlations extends Base_WP_Manga_Site_Model {
-	public $baseURL = 'https://trashscanlations.com/';
+	public $baseURL = 'https://trashscanlations.com';
 	public $titleStub = 'series';
 }
