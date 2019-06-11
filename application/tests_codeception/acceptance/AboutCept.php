f7b62b44c00930a0d8e738901905e9b0e59e2608
Angus Johnston <dakutree@codeanimu.net>
2019-06-11 20:37:14 +0100

    remove unused codeception tests

--- application/tests_codeception/acceptance/AboutCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$I = new AcceptanceTester($scenario);
-
-	$I->wantTo('Check the About Page works');
-	$I->amOnPage('/about');
-	$I->see('This is a site dedicated to tracking manga across multiple aggregate sites.');
-	$I->see('©2016 trackr.moe');
-	$I->seeInTitle('Manga Tracker - About');
